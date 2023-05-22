582b2bb3ab516526977473deb7176f236882edba
Teodor G <source@teogor.dev>
2023-05-22 23:51:22 +0300

    fixed demo content

--- src/main/kotlin/dev/teogor/Main.kt
+++ src/main/kotlin/dev/teogor/Main.kt
@@ -3,7 +3,10 @@ package dev.teogor
 import dev.teogor.pixel.harvest.PixelHarvestBot
 import io.github.cdimascio.dotenv.dotenv
 
-// https://discord.com/oauth2/authorize?client_id=1110114222442033172&/scope=bot&permissions=8
+// todo discord bot info pages
+//  https://discord.com/developers/applications/1110114222442033172/information
+//  https://discord.com/oauth2/authorize?client_id=1110114222442033172&/scope=bot&permissions=8
+
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
