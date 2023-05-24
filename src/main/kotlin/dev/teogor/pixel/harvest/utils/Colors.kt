fe0c2b56e3f78869cdf8e2e5b93367cbb8f3f17d
Teodor G <source@teogor.dev>
2023-05-24 16:36:24 +0300

    created colors class

---
+++ src/main/kotlin/dev/teogor/pixel/harvest/utils/Colors.kt
@@ -0,0 +1,33 @@
+package dev.teogor.pixel.harvest.utils
+
+import dev.kord.common.Color
+
+object Colors {
+    val BLACK = Color(0, 0, 0)
+    val BLUE = Color(0, 0, 255)
+    val CYAN = Color(0, 255, 255)
+    val GRAY = Color(128, 128, 128)
+    val GREEN = Color(0, 128, 0)
+    val LIME = Color(0, 255, 0)
+    val MAGENTA = Color(255, 0, 255)
+    val MAROON = Color(128, 0, 0)
+    val NAVY = Color(0, 0, 128)
+    val OLIVE = Color(128, 128, 0)
+    val PURPLE = Color(128, 0, 128)
+    val RED = Color(255, 0, 0)
+    val SILVER = Color(192, 192, 192)
+    val TEAL = Color(0, 128, 128)
+    val WHITE = Color(255, 255, 255)
+    val YELLOW = Color(255, 255, 0)
+
+    val INDIGO = Color(75, 0, 130)
+    val ORANGE = Color(255, 165, 0)
+    val VIOLET = Color(238, 130, 238)
+    val YELLOW_GREEN = Color(154, 205, 50)
+    val SKY_BLUE = Color(135, 206, 235)
+    val ROYAL_BLUE = Color(65, 105, 225)
+    val SPRING_GREEN = Color(0, 255, 127)
+    val DEEP_PINK = Color(255, 20, 147)
+    val DARK_ORCHID = Color(153, 50, 204)
+    val CORNFLOWER_BLUE = Color(100, 149, 237)
+}
