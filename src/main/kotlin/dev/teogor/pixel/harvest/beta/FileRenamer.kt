25c40471f7be8fc6c6e9edecf74cc06e61d5174c
Teodor G <source@teogor.dev>
2023-07-15 16:51:03 +0300

    Update FileRenamer.kt

--- src/main/kotlin/dev/teogor/pixel/harvest/beta/FileRenamer.kt
+++ src/main/kotlin/dev/teogor/pixel/harvest/beta/FileRenamer.kt
@@ -4,31 +4,59 @@ import dev.teogor.pixel.harvest.discord.PathUtils
 import dev.teogor.pixel.harvest.svg.SvgConverter
 import java.io.File
 
-fun main() {
-    val baseDownloadPath = "PixelHarvest\\ZeoAI-Automation\\images"
-    // val folderPath = "${PathUtils.getDownloadsFolderPath()}\\$baseDownloadPath"
-    val folderPath = "E:\\Adobe Stock\\Iconiq Hub\\Images\\set-000001\\split"
+class FileRenamer private constructor(
+    val directory: File,
+    val searchPattern: String,
+    val replacement: String,
+) {
 
-    val searchPattern = "l00"
-    val replacement = "l_00"
-
-    val folder = File(folderPath)
-
-    if (folder.exists() && folder.isDirectory) {
-        val files = folder.listFiles()
-        files?.forEach { file ->
-            val originalName = file.name
-            val newName = originalName.replace(searchPattern, replacement)
-            if (newName != originalName) {
-                val renamedFile = File(folder, newName)
-                if (file.renameTo(renamedFile)) {
-                    println("Renamed file: $originalName -> $newName")
-                } else {
-                    println("Failed to rename file: $originalName")
-                }
+    companion object {
+        fun forDirectory(
+            directory: File,
+            searchPattern: String,
+            replacement: String,
+        ): FileRenamer {
+            return FileRenamer(
+                directory = directory,
+                searchPattern = searchPattern,
+                replacement = replacement,
+            ).apply {
+                renameWithinDirectory()
             }
         }
-    } else {
-        println("Invalid folder path: $folderPath")
     }
+
+    private fun renameWithinDirectory() {
+        if (directory.exists() && directory.isDirectory) {
+            val files = directory.listFiles()
+            files?.forEach { file ->
+                val originalName = file.name
+                val newName = originalName.replace(searchPattern, replacement)
+                if (newName != originalName) {
+                    val renamedFile = File(directory, newName)
+                    if (file.renameTo(renamedFile)) {
+                        println("Renamed file: $originalName -> $newName")
+                    } else {
+                        println("Failed to rename file: $originalName")
+                    }
+                }
+            }
+        } else {
+            println("Invalid folder path: ${directory.absolutePath}")
+        }
+    }
+}
+
+fun main() {
+    // sof::customizable
+    val directoryPath = "${PathUtils.getDownloadsFolderPath()}\\PixelHarvest\\ZeoAI-Automation\\images\\photo-ai"
+    val searchPattern = "Generate "
+    val replacement = ""
+    // eof::customizable
+
+    FileRenamer.forDirectory(
+        directory = File(directoryPath),
+        searchPattern = searchPattern,
+        replacement = replacement,
+    )
 }
